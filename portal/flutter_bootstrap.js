b8c91b898c77b7f9a5b2a6f316d05dc42d053e25
Trance-0 <Trance-0@users.noreply.github.com>
2026-06-23 22:40:52 +0000

    deploy: 59ecfd4905ec4d23d07bc8e99181ca5405bed97f

--- portal/flutter_bootstrap.js
+++ portal/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "431959979" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3105579627" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
