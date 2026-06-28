87da0129ff8161690b6686f93bc68e2e8e1bdfd4
Trance-0 <Trance-0@users.noreply.github.com>
2026-06-28 15:18:04 +0000

    deploy: ce41a0b70ed2f902c349a744dfeab0cb228bc096

--- planner/flutter_bootstrap.js
+++ planner/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f8d","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}],"useLocalCanvasKit":true};
+_flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc030","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}],"useLocalCanvasKit":true};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "170163202" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2594108288" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
